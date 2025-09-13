da560e29d1d0dc323b5926dcd054b116df712f13
ulrich <koudjouulrich@gmail.com>
2025-09-13 19:01:59 +0100

    test

--- src/MesRoutes.jsx
+++ src/MesRoutes.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Home from './common/page/home';
 import TutorForm from './common/page/TutorForm';
 import StudentForm from './common/page/StudentForm';
 import StudentsListe from './common/page/ListeStudent';
 import Createdatestudent from './common/page/createavailidedate';
 import MatchesTable from './common/page/ListeDIsponiliteprof';
 import TutorsListe from './common/page/ListeProf';
+import Home from './common/page/Home';
 
 const MesRoutes = () => {
     return (
